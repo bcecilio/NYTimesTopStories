1fa3a25ce800f502a79d6d9afa48ac7c45a43d32
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-06 10:50:03 -0500

    Initial Commit

---
+++ NYTimesTopStories/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  NYTimesTopStories
+//
+//  Created by Brendon Cecilio on 2/6/20.
+//  Copyright © 2020 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    }
+}
