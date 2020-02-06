4de3cc30dd0b659644f046e119f34ec24dbe731f
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-06 12:35:09 -0500

    "wrote unit test against TopStory model"

--- NYTimesTopStories/Controllers/NewsFeedController.swift
+++ NYTimesTopStories/Controllers/NewsFeedController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class NewsFeedController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+    private let newsFeed = NewsFeedView()
+    
+    override func loadView() {
+        view = newsFeed
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        newsFeed.collectionView.delegate = self
+        newsFeed.collectionView.dataSource = self
+        newsFeed.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "newsCell")
+    }
+}
+
+extension NewsFeedController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "newsCell", for: indexPath)
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemHeight: CGFloat = maxSize.height * 0.30
+        let itemWidth: CGFloat = maxSize.width
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
 }
